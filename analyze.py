2015d64e8016ba84fb1bab381f28c681ae2a6b84
zmunro <zacharymunro2@gmail.com>
2020-11-05 21:45:14 -0500

    added more analysis options

--- analyze.py
+++ analyze.py
@@ -1,56 +1,53 @@
 import json
+import sys
+
 import fire
 
 
-def main(state: str = None, min_votes: int = 5_000):
+def main(
+    state: str = None, min_votes: int = 5_000, by_state: str = "yes", candidate: str = "Joe Biden"
+):
+    candidates = set()
     with open("output.json", "r") as in_f:
         data = json.load(in_f)
-    choice_state = {}
-    dem_by_state = {}
+    candidate_by_state = {}
     for state_name, state_data in data.items():
-        max_percent_dem = 0
-        max_county = ""
-        max_state = ""
-        dem_votes = 0
-        max_dem_votes = 0
         for county_name, county_data in state_data.items():
             num_votes = sum(county_data.values())
-            dem_votes = county_data["Joe Biden"]
-            if num_votes == 0:
-                continue
-            percent_dem = county_data["Joe Biden"] / num_votes
-            if percent_dem > max_percent_dem and num_votes >= min_votes:
-                max_county_data = county_data
-                max_dem_votes = dem_votes
-                max_percent_dem = percent_dem
-                max_county = county_name
-            if state is not None and state_name == state:
-                choice_state[county_name] = {
-                    "percent_dem": percent_dem,
-                    "county_name": county_name,
-                    "dem_votes": dem_votes,
-                }
-        dem_by_state[state_name] = {
-            "max_dem_votes": max_dem_votes,
-            "max_percent_dem": max_percent_dem,
-            "max_county": max_county,
-        }
 
-    if state is not None:
-        choice_state_sorted = sorted(
-            choice_state.items(), key=lambda x: x[1]["percent_dem"], reverse=True
-        )
-        for i in choice_state_sorted:
+            if candidate not in county_data.keys() or num_votes == 0:
+                continue
+
+            candidate_votes = county_data[candidate]
+
+            candidates = candidates.union(set(county_data.keys()))
+            percent_candidate = county_data[candidate] / num_votes
+            if num_votes >= min_votes:
+                candidate_by_state[(state_name, county_name)] = {
+                    "candidate_votes": candidate_votes,
+                    "percent_candidate": percent_candidate,
+                    "county": county_name,
+                    "state": state_name,
+                }
+
+    candidate_by_state_sorted = sorted(
+        candidate_by_state.items(), key=lambda x: x[1]["percent_candidate"], reverse=True
+    )
+    print("state,precinct,percent_candidate,candidate_votes")
+    for i in candidate_by_state_sorted:
+        if state is None or i[0][0] == state:
             print(
-                f"{i[0]}: percent_dem - {round(i[1]['percent_dem'] * 100)}%, county - {i[1]['county_name']}"
-            )
-    else:
-        sorted_states = sorted(
-            dem_by_state.items(), key=lambda x: x[1]["max_percent_dem"], reverse=True
-        )
-        for i in sorted_states:
-            print(
-                f"{i[0]}: percent_dem - {round(i[1]['max_percent_dem'] * 100)}%, county - {i[1]['max_county']}"
+                ",".join(
+                    [
+                        str(x)
+                        for x in [
+                            i[0][0],
+                            i[0][1],
+                            round(100 * i[1]["percent_candidate"]),
+                            i[1]["candidate_votes"],
+                        ]
+                    ]
+                )
             )
 
 
